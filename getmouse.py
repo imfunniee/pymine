5de5e0eb5b2a5845fa06cce927c65b286088794f
imfunny <36105478+imfunniee@users.noreply.github.com>
2018-12-15 19:47:16 +0530

    Add files via upload
    will be used to for mouse movements ;)

--- getmouse.py
+++ getmouse.py
@@ -1,6 +1,8 @@
 import pyautogui, sys
 try:
     while True:
+        #moves your mouse ---> pyautogui.moveTo(100, 200)
+        #gets direction
         x, y = pyautogui.position()
         positionStr = 'mouse-x: ' + str(x).rjust(4) + ' mouse-y: ' + str(y).rjust(4)
         print(positionStr, end='')
