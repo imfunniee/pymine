01b4f8e38113e47e32f2a10b62c9e399f0a6142b
imfunny <36105478+imfunniee@users.noreply.github.com>
2018-12-15 21:38:31 +0530

    Add files via upload

--- getmouse.py
+++ getmouse.py
@@ -5,7 +5,7 @@ window_width =  GetSystemMetrics(0)
 direction = 0
 position = 0
 
-try:
+def mouse():
     while True:
         #gets direction
         x, y = pyautogui.position() #y could be used for looking up idk but for now meh
@@ -26,12 +26,9 @@ try:
             else:
                 direction = 0
                 position = 0
-        print(direction)
-except KeyboardInterrupt:
-    print('\n')
+    return direction
 
 #0 = mouse doesn't move
 #1 = move left
 #2 = moved right
-
 #pyautogui.moveTo(100, 100, 2, pyautogui.easeOutQuad)
