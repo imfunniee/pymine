7a85e837089a191c8dea00bda3b0b635b9d451b7
imfunny <36105478+imfunniee@users.noreply.github.com>
2018-12-17 17:20:55 +0530

    Add files via upload

--- collectdata.py
+++ collectdata.py
@@ -3,18 +3,27 @@ from grabscreen import grab_screen
 import cv2
 import time
 from getkeys import key_check
+from getmouse import check_mouse
 import os
 
-
 def keys_to_output(keys):
-    output = [0,0,0]
+    #[left,forward,right,down,jump,mouse_movement_left,mouse_movement_right)]
+    output = [0,0,0,0,0,0,0]
     
     if 'A' in keys:
         output[0] = 1
+    elif 'W' in keys:
+        output[1] = 1
     elif 'D' in keys:
         output[2] = 1
-    else:
-        output[1] = 1
+    elif 'S' in keys:
+        output[3] = 1
+    elif ' ' in keys:
+        output[4] = 1
+    elif 1 in keys:
+        output[5] = 1
+    elif 2 in keys:
+        output[6] = 1
     return output
 
 
@@ -26,7 +35,6 @@ if os.path.isfile(file_name):
 else:
     print('File does not exist, starting fresh!')
     training_data = []
-
 
 def main():
 
@@ -40,12 +48,14 @@ def main():
 
         if not paused:
             # 800x600 windowed mode
-            screen = grab_screen(region=(0,40,800,460))
+            screen = grab_screen(region=(0,40,800,640))
             last_time = time.time()
             screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-            screen = cv2.resize(screen, (80,46))
+            screen = cv2.resize(screen, (80,60))
             # resize to something a bit more acceptable for a CNN
             keys = key_check()
+            mouse_movement = check_mouse()
+            keys.append(mouse_movement)
             output = keys_to_output(keys)
             training_data.append([screen,output])
             
@@ -63,6 +73,5 @@ def main():
                 print('Pausing!')
                 paused = True
                 time.sleep(1)
-
 
 main()
